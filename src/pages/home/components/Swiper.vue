b4c7cbf8d4f39501d877894f7690ca08a871721f
NoglaUJ <s984833@gmail.com>
2020-01-17 17:54:34 +0800

    swiper-change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" >
+    </swiper-slide>
+  <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default{
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/5e3978fe497a750cfe44849159d078f7.jpg_750x200_98de326a.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/0b26e9760c2673c63c05e1efce1275ea.jpg_750x200_495c0a72.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper>>>.swiper-pagination-bullet-active
+  background:purple
+.wrapper
+  width: 100%
+  height: 0
+  padding-bottom:27%
+  background:#eee
+  .swiper-img
+    width:100%
+</style>
