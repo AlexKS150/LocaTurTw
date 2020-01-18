ac051d58a9fbc3ded6fe4b54fe31ff85238ba3c4
NoglaUJ <s984833@gmail.com>
2020-01-18 17:58:11 +0800

    city search

---
+++ src/pages/city/component/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="輸入縣市名稱">
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeSearch',
+  props: {
+    city: String
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+.search
+  height:.72rem
+  padding:0 0.2rem
+  background:$bgColor
+  .search-input
+    box-sizing:border-sizing
+    width:100%
+    height:.62rem
+    padding:0 0.05rem
+    line-height:.62rem
+    text-align:center
+    border-radius:.06rem
+    color:#666
+
+</style>
