0db78060f1f4bef7319a16ea7d68b982514b69ab
林鈺堂 <linyutang@linyutangdeMacBook-Air.local>
2020-01-19 17:18:36 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,24 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-sidelist></city-sidelist>
 </div>
 </template>
 
 <script>
 import CityHeader from './component/Header'
 import CitySearch from './component/Search'
+import CityList from './component/List'
+import CitySidelist from './component/SideList'
 
 export default{
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CitySidelist
   }
 }
 </script>
---
+++ src/pages/city/component/SideList.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list">
+        <li class="item">北</li>
+        <li class="item">中</li>
+        <li class="item">南</li>
+        <li class="item">東</li>
+        <li class="item">島</li>
+    </ul>
+</template>
+
+<script>
+export default{
+  name: 'CitySidelist'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+.list
+    display:flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    .item
+        line-height: .44rem
+        text-align: center
+        color:$bgColor
+</style>
