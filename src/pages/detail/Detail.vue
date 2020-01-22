3ec1500db35238f51860b4db6412e8450bd63297
linyutang <s984833@gmail.com>
2020-01-22 14:54:14 +0800

    detail list add

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+          <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '一般票',
+        children: [{
+          title: '第三方案票',
+          children: [{
+            title: '第三方案-無購物點票'
+          }]
+        }, {
+          title: '第五方案票'
+        }]
+      }, {
+        title: '優惠票'
+      }, {
+        title: '學生票'
+      }, {
+        title: '兒童票'
+      }]
+    }
   }
 }
 </script>
