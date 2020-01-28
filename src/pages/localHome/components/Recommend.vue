dc3a2de48376d69036349e9010cde34a080bbc8e
linyutang <s984833@gmail.com>
2020-01-28 09:51:50 +0800

    detail taipei id:0 done

--- src/pages/localHome/components/Recommend.vue
+++ src/pages/localHome/components/Recommend.vue
@@ -2,16 +2,16 @@
   <div>
     <div class="title">推薦地點</div>
     <ul>
-      <router-link tag="li"  class="li-item" v-for="item of list" :key="item.detailID" :to=" '/localDetail/' + item.detailID ">
-      <div class="click-change" @click="handleDetailIDChange(item.detailID)">
+      <li class="li-item" v-for="item of list" :key="item.detailID">
           <img class="item-img" :src="item.imgUrl">
           <div class="item-info">
             <p class="item-title">{{item.title}}</p>
             <p class="item-desc">{{item.desc}}</p>
-            <button class="item-button">查看詳情</button>
+            <router-link tag="button" class="item-button" :to=" '/localDetail/' + item.detailID">
+              <div class="click-change" @click="handleDetailIDChange(item.detailID)">查看詳情</div>
+            </router-link>
           </div>
-        </div>
-      </router-link>
+      </li>
     </ul>
   </div>
 </template>
@@ -49,27 +49,26 @@ export default{
   overflow:hidden
   display:flex
   height:1.9rem
-  .click-change
-    .item-img
-      width:1.7rem
-      height:1.7rem
-      padding:.1rem
-    .item-info
-      float:right
-      padding:.1rem
-      min-width:0
-      .item-title
-        line-height:.54rem
-        font-size:.32rem
-        ellipsis()
-      .item-desc
-        line-height:.4rem
-        color:blue
-        ellipsis()
-      .item-button
-        line-height:.44rem
-        margin-top:.16rem
-        background:#ff9300
-        padding:0.1rem
-        border-radius:.06rem
+  .item-img
+    width:1.7rem
+    height:1.7rem
+    padding:.1rem
+  .item-info
+    flex:1
+    padding:.1rem
+    min-width:0
+    .item-title
+      line-height:.54rem
+      font-size:.32rem
+      ellipsis()
+    .item-desc
+      line-height:.4rem
+      color:blue
+      ellipsis()
+    .item-button
+      line-height:.44rem
+      margin-top:.16rem
+      background:#ff9300
+      padding:0.1rem
+      border-radius:.06rem
 </style>
